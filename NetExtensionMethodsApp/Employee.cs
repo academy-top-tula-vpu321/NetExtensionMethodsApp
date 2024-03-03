817bd9562548a30089dc3a7dee0c5aec3cea1ce6
maxefimov <efimov.top.academy@gmail.com>
2024-03-03 09:50:59 +0300

    Добавьте файлы проекта.

---
+++ NetExtensionMethodsApp/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExtensionMethodsApp
+{
+    public partial class Employee
+    {
+        public string? Name { get; set; }
+        public Employee(string? name)
+        {
+            Name = name;
+        }
+
+        public partial void Print();
+    }
+}
---
+++ NetExtensionMethodsApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using NetExtensionMethodsApp;
+
+string str = "Hello world";
+//Console.WriteLine(StringAdvansed.CountChar(str, 'l'));
+Console.WriteLine(str.CountChar('l'));
+
+Employee bob = new Employee("Bobby");
+bob.Print();
+Employee tom = new Employee("Tommy", 32);
+tom.Print();
+
+
+namespace NetExtensionMethodsApp
+{
+    public static class StringExtension
+    {
+        public static int CountChar(this string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+                if (c == ch) count++;
+            return count;
+        }
+    }
+
+    public partial class Employee
+    {
+        public int Age { get; set; } = 0;
+        public Employee(string name, int age) : this(name)
+        {
+            this.Age = age;
+        }
+
+        public partial void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+
+
+    //static class StringAdvansed
+    //{
+    //    public static int CountChar(string str, char ch)
+    //    {
+    //        int count = 0;
+    //        foreach (char c in str)
+    //            if (c == ch) count++;
+    //        return count;
+    //    }
+    //}
+
+}
